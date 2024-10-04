9d4646bf6e6f24c9ecbffbf45b4d7ffddf7ece0b
DevPBDias <devpbdias@gmail.com>
2024-10-04 11:58:42 -0300

    feat: created hero text and section for cards, and styled it

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -16,18 +16,29 @@ const Dashboard = () => {
           priority
         />
       </picture>
-      <section className="text-container">
-        <h1>O lugar onde descobre o que há de melhor no mundo da música</h1>
-        <Link href="/dashboard/music">
-          <p>Descubra</p>
-          <ArrowUpRight size={24} color="#EBFFEB" />
-        </Link>
+      <section className="container-content">
+        <section className="container-seach-date">
+          <SearchBar />
+          <DateAndWheater />
+        </section>
+        <section className="text-container">
+          <h1>
+            O lugar onde descobre o
+            <br />
+            que há de melhor no
+            <br />
+            mundo da música
+          </h1>
+          <Link href="/dashboard/music">
+            <p>Descubra</p>
+            <ArrowUpRight size={24} color="#EBFFEB" />
+          </Link>
+        </section>
+        <section className="container-musics">
+          <h3>Descubra novas músicas todo dia</h3>
+          <div>card</div>
+        </section>
       </section>
-      <section className="container-seach-date">
-        <SearchBar />
-        <DateAndWheater />
-      </section>
-      Dashboard
     </main>
   );
 };
