2fc9facfc1cb86d68eb56d099c17a54f810eb5b3
DevPBDias <devpbdias@gmail.com>
2024-10-04 10:02:13 -0300

    feat/fix: added hero image for dashboard page and fixed styles for containers

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,24 @@
 import DateAndWheater from "@/components/dateAndWheater";
 import SearchBar from "@/components/searchBar";
+import Image from "next/image";
+import heroImg from "@/assets/images/main-hero.png";
 
 const Dashboard = () => {
   return (
     <main className="container-dashboard">
-      Dashboard
+      <picture className="hero-container-image">
+        <Image
+          className="hero-image"
+          src={heroImg}
+          alt="main hero image"
+          priority
+        />
+      </picture>
       <section className="container-seach-date">
         <SearchBar />
         <DateAndWheater />
       </section>
+      Dashboard
     </main>
   );
 };
