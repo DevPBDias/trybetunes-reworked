755d2dd467eb96c49dd71572411bde4c0f02ec95
DevPBDias <devpbdias@gmail.com>
2024-10-04 10:13:06 -0300

    created: text hero section and styled it

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,8 @@ import DateAndWheater from "@/components/dateAndWheater";
 import SearchBar from "@/components/searchBar";
 import Image from "next/image";
 import heroImg from "@/assets/images/main-hero.png";
+import Link from "next/link";
+import { ArrowUpRight } from "lucide-react";
 
 const Dashboard = () => {
   return (
@@ -14,6 +16,13 @@ const Dashboard = () => {
           priority
         />
       </picture>
+      <section className="text-container">
+        <h1>O lugar onde descobre o que há de melhor no mundo da música</h1>
+        <Link href="/dashboard/music">
+          <p>Descubra</p>
+          <ArrowUpRight size={24} color="#EBFFEB" />
+        </Link>
+      </section>
       <section className="container-seach-date">
         <SearchBar />
         <DateAndWheater />
