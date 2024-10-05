3b4e58bec26f172101fafeec5da4a52dc2590dce
DevPBDias <devpbdias@gmail.com>
2024-10-05 12:32:21 -0300

    feat: styled profile page and added new user image

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -4,7 +4,7 @@ import DateAndWheater from "@/components/dateAndWheater";
 import SearchBar from "@/components/searchBar";
 import { useState } from "react";
 import "./profile.scss";
-import user from "@/assets/images/user.png";
+import user from "@/assets/images/user-big.png";
 import Image from "next/image";
 import { Clipboard, Save } from "lucide-react";
 
@@ -23,32 +23,47 @@ const ProfilePage = () => {
       <h3>Editar perfil</h3>
       <form className="container-form">
         <picture className="user-container-image">
-          <Image className="user-image" src={user} alt="user image" priority />
+          <Image
+            className="user-image"
+            src={user}
+            alt="user image"
+            quality={100}
+            priority
+          />
         </picture>
-        <section>
-          <fieldset className="container-field">
-            <label htmlFor="firstName">Nome</label>
-            <input
-              type="text"
-              name="firstName"
-              id="firstName"
-              value={firstName}
-            />
-          </fieldset>
-          <fieldset className="container-field">
-            <label htmlFor="lastName">Sobrenome</label>
-            <input type="text" name="lastName" id="lastName" value={lastName} />
-          </fieldset>
-          <fieldset className="container-field">
-            <label htmlFor="email">E-mail</label>
-            <input type="text" name="email" id="email" value={email} />
-          </fieldset>
-          <fieldset className="container-field">
-            <label htmlFor="adress">Endereço</label>
-            <input type="text" name="adress" id="adress" value={adress} />
-          </fieldset>
+        <section className="container-inputs">
+          <div className="container-row-inputs">
+            <fieldset className="container-field">
+              <label htmlFor="firstName">Nome</label>
+              <input
+                type="text"
+                name="firstName"
+                id="firstName"
+                value={firstName}
+              />
+            </fieldset>
+            <fieldset className="container-field">
+              <label htmlFor="lastName">Sobrenome</label>
+              <input
+                type="text"
+                name="lastName"
+                id="lastName"
+                value={lastName}
+              />
+            </fieldset>
+          </div>
+          <div className="container-row-inputs">
+            <fieldset className="container-field">
+              <label htmlFor="email">E-mail</label>
+              <input type="text" name="email" id="email" value={email} />
+            </fieldset>
+            <fieldset className="container-field">
+              <label htmlFor="adress">Endereço</label>
+              <input type="text" name="adress" id="adress" value={adress} />
+            </fieldset>
+          </div>
         </section>
-        <section>
+        <section className="container-btns">
           <button className="btn-profile" type="button">
             <p>Limpar</p>
             <Clipboard size={24} color="#001400" />
