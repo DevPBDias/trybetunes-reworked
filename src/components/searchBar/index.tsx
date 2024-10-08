edd6ee83b4981279d7e2816e2c4f871374962604
DevPBDias <devpbdias@gmail.com>
2024-10-08 11:53:38 -0300

    feat: used form validation for search bar

--- src/components/searchBar/index.tsx
+++ src/components/searchBar/index.tsx
@@ -3,20 +3,49 @@
 import { Search } from "lucide-react";
 import "./styles.scss";
 import { useRouter } from "next/navigation";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { searchSchema } from "@/schemas";
+import { useMusicContext } from "@/context/music-provider";
+
+type FormData = z.infer<typeof searchSchema>;
 
 const SearchBar = () => {
-  const route = useRouter();
+  const { setSearchedName } = useMusicContext();
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitSuccessful },
+    reset,
+  } = useForm<FormData>({
+    resolver: zodResolver(searchSchema),
+  });
+
+  const handleSearch = (data: FormData) => {
+    setSearchedName(data.search);
+    console.log("clickou");
+    if (isSubmitSuccessful) {
+      reset({ search: "" });
+    }
+    router.push("/dashboard/search");
+  };
 
   return (
-    <form className="form-searchbar">
+    <form className="form-searchbar" onSubmit={handleSubmit(handleSearch)}>
       <fieldset>
         <input
+          className={`${errors.search ? "error-border" : ""}`}
           type="text"
-          name="search"
+          {...register("search")}
           placeholder="Pesquise pelo seus artistas preferidos...."
         />
+        {errors.search && (
+          <span className="error-msg">{errors.search.message}</span>
+        )}
       </fieldset>
-      <button type="button" onClick={() => route.push("/dashboard/search")}>
+      <button type="submit">
         <Search size={24} color="#001400" />
       </button>
     </form>
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -2,5 +2,11 @@ import { z } from "zod";
 
 export const inputSchema = z.object({
     email: z.string().email('E-mail inválido, por favor corrija-o.'),
-    password: z.string().min(3, 'Senha curta, por favor insira mais caracteres.')
+    password: z.string().min(3, 'Senha curta, por favor insira mais caracteres.'),
+    search: z.string().min(3, 'Nome curto, por favor insira mais caracteres.')
+  });
+
+
+  export const searchSchema = z.object({
+    search: z.string().min(3, 'Nome curto, por favor insira mais caracteres.')
   });
