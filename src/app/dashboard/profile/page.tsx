3f716a1c49167e2110a1d11c005ba99c2a4970c7
DevPBDias <devpbdias@gmail.com>
2024-10-23 11:35:02 -0300

    feat: made profile form dynamic

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -7,27 +7,30 @@ import Image from "next/image";
 import { Clipboard, Save } from "lucide-react";
 import { registerSchema } from "@/schemas";
 import { z } from "zod";
-import { useForm } from "react-hook-form";
+import { useForm, FormProvider } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
 import { useUserContext } from "@/context/user-provider";
 import "./profile.scss";
+import { Form } from "@/components/form";
 
-type FormData = z.infer<typeof registerSchema>;
+type ProfileData = z.infer<typeof registerSchema>;
 
 const ProfilePage = () => {
   const router = useRouter();
   const { setUserName } = useUserContext();
+  const profileForm = useForm<ProfileData>({
+    resolver: zodResolver(registerSchema),
+  });
+
   const {
     register,
     handleSubmit,
     formState: { errors, isSubmitSuccessful },
     reset,
-  } = useForm<FormData>({
-    resolver: zodResolver(registerSchema),
-  });
+  } = profileForm;
 
-  const handleEdit = (data: FormData) => {
+  const handleEdit = (data: ProfileData) => {
     localStorage.setItem("loggedUser", JSON.stringify(data));
     localStorage.setItem("trybetunes-users", JSON.stringify(data));
     const joinName = data.firstName.concat(" ", data.lastName);
@@ -45,87 +48,77 @@ const ProfilePage = () => {
         <DateAndWheater />
       </section>
       <h3>Editar perfil</h3>
-      <form className="container-form" onSubmit={handleSubmit(handleEdit)}>
-        <picture className="user-container-image">
-          <Image
-            className="user-image"
-            src={user}
-            alt="user image"
-            quality={100}
-            priority
-          />
-        </picture>
-        <section className="container-inputs">
-          <div className="container-row-inputs">
-            <fieldset className="container-field">
-              <label htmlFor="firstName">Nome</label>
-              <input
-                type="text"
-                className={`${errors.firstName ? "error-border" : ""}`}
-                {...register("firstName")}
-                id="firstName"
-              />
-              {errors.firstName && (
-                <span className="error-msg">{errors.firstName.message}</span>
-              )}
-            </fieldset>
-            <fieldset className="container-field">
-              <label htmlFor="lastName">Sobrenome</label>
-              <input
-                type="text"
-                className={`${errors.lastName ? "error-border" : ""}`}
-                {...register("lastName")}
-                id="lastName"
-              />
-              {errors.lastName && (
-                <span className="error-msg">{errors.lastName.message}</span>
-              )}
-            </fieldset>
-          </div>
-          <div className="container-row-inputs">
-            <fieldset className="container-field">
-              <label htmlFor="email">E-mail</label>
-              <input
-                type="text"
-                className={`${errors.email ? "error-border" : ""}`}
-                {...register("email")}
-                id="email"
-              />
-              {errors.email && (
-                <span className="error-msg">{errors.email.message}</span>
-              )}
-            </fieldset>
-            <fieldset className="container-field">
-              <label htmlFor="adress">Senha</label>
-              <input
-                type="text"
-                className={`${errors.password ? "error-border" : ""}`}
-                {...register("password")}
-                id="adress"
-              />
-              {errors.password && (
-                <span className="error-msg">{errors.password.message}</span>
-              )}
-            </fieldset>
-          </div>
-        </section>
-        <section className="container-btns">
-          <button
-            className="btn-profile"
-            type="button"
-            onClick={() =>
-              reset({ email: "", firstName: "", lastName: "", password: "" })
-            }
-          >
-            <p>Limpar</p>
-            <Clipboard size={24} color="#001400" />
-          </button>
-          <button className="btn-profile main-btn" type="submit">
-            <p>Salvar</p>
-            <Save size={24} color="#EBFFEB" />
-          </button>
-        </section>
-      </form>
+      <FormProvider {...profileForm}>
+        <form className="container-form" onSubmit={handleSubmit(handleEdit)}>
+          <picture className="user-container-image">
+            <Image
+              className="user-image"
+              src={user}
+              alt="user image"
+              quality={100}
+              priority
+            />
+          </picture>
+          <section className="container-inputs">
+            <div className="container-row-inputs">
+              <Form.Field className="container-field">
+                <Form.Label htmlFor="firstName">Nome</Form.Label>
+                <Form.Input
+                  className={`${errors.firstName ? "error-border" : ""}`}
+                  type="text"
+                  name="firstName"
+                />
+                <Form.ErrorMessage field="firstName" />
+              </Form.Field>
+              <Form.Field className="container-field">
+                <Form.Label htmlFor="lastName">Sobrenome</Form.Label>
+                <Form.Input
+                  className={`${errors.lastName ? "error-border" : ""}`}
+                  type="text"
+                  name="lastName"
+                />
+                <Form.ErrorMessage field="lastName" />
+              </Form.Field>
+            </div>
+            <div className="container-row-inputs">
+              <Form.Field className="container-field">
+                <Form.Label htmlFor="email">E-mail</Form.Label>
+                <Form.Input
+                  className={`${errors.email ? "error-border" : ""}`}
+                  type="text"
+                  name="email"
+                />
+                <Form.ErrorMessage field="email" />
+              </Form.Field>
+              <Form.Field className="container-field">
+                <Form.Label htmlFor="password">Senha</Form.Label>
+                <Form.Input
+                  className={`${errors.password ? "error-border" : ""}`}
+                  type="text"
+                  name="password"
+                />
+                <Form.ErrorMessage field="password" />
+              </Form.Field>
+            </div>
+          </section>
+          <section className="container-btns">
+            <Form.Button
+              className="btn-profile"
+              type="button"
+              onClick={() =>
+                reset({ email: "", firstName: "", lastName: "", password: "" })
+              }
+            >
+              <p>Limpar</p>
+              <Clipboard size={24} color="#001400" />
+            </Form.Button>
+            <Form.Button className="btn-profile main-btn" type="submit">
+              <p>Salvar</p>
+              <Save size={24} color="#EBFFEB" />
+            </Form.Button>
+          </section>
+        </form>
+      </FormProvider>
     </main>
   );
 };
